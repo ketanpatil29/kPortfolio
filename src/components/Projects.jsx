27fca159000bea1ab8343454d772fe7f8bf87504
Ketan patil <ketanpatil.sh@gmail.com>
2025-10-10 11:52:29 +0530

    Added pages

---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Projects = () => {
+    return (
+        <section>
+            <div className="text-[#a9a9b3] flex flex-col items-center justify-center mt-16">
+                <h1 className="font-bol text-4xl">Hello this is all are my projects that i have made</h1>
+            </div>
+        </section>
+    );
+}
+
+export default Projects;
